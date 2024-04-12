50bc03ab2b335e4fda3869b139720235a379cce8
unknown <gabrielquadro@gmail.com>
2024-04-11 23:21:46 -0300

    Create structure controllers and service

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,13 @@
 import { Router , Response , Request} from "express"
+import { CreateUserController } from "./controllers/user/CreateUserController";
 
 const router = Router();
 
 router.get("/teste", ( req : Request, res : Response) => {
     return res.json({ ok : true })
 })
+
+//User routes
+router.post("/user", new CreateUserController().handle)
 
 export {router}
