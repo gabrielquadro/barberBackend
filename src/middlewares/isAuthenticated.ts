ed80fb2069be4b1d7a04b25d77d7a2a66bbb6071
unknown <gabrielquadro@gmail.com>
2024-04-12 22:00:19 -0300

    Middleware, JWT and UserDetails

---
+++ src/middlewares/isAuthenticated.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import { verify } from "jsonwebtoken";
+
+interface Payload {
+    sub: string,
+}
+
+export function isAuthenticated(
+    request: Request,
+    response: Response,
+    next: NextFunction
+) {
+    const authToken = request.headers.authorization;
+
+    if (!authToken) {
+        return response.status(401).end();
+    }
+
+    const [, token] = authToken.split(" ")
+
+    try {
+        const { sub } = verify(
+            token,
+            process.env.JWT_SECRET
+        ) as Payload;
+
+        request.user_id = sub; //id do user logado
+
+        return next();
+
+    } catch (err) {
+        return response.status(401).end();
+    }
+
+    console.log(token);
+}
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
-import { Router , Response , Request} from "express"
+import { Router, Response, Request } from "express"
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
-
+import { DetailUserController } from "./controllers/user/DetailUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
 const router = Router();
 
 //User routes
 router.post("/user", new CreateUserController().handle)
 router.post("/session", new AuthUserController().handle)
+router.get("/me", isAuthenticated , new DetailUserController().handle)
 
-export {router}
+export { router }
