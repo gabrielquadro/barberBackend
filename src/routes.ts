33ba91bcc8b8d305240842fdfe8a1b49c6cdcfaa
unknown <gabrielquadro@gmail.com>
2024-04-12 22:45:00 -0300

    Update user

--- src/routes.ts
+++ src/routes.ts
@@ -3,11 +3,14 @@ import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { UpdateUserController } from "./controllers/user/UpdateUserController";
+
 const router = Router();
 
 //User routes
 router.post("/user", new CreateUserController().handle)
 router.post("/session", new AuthUserController().handle)
 router.get("/me", isAuthenticated , new DetailUserController().handle)
+router.put("/user", isAuthenticated , new UpdateUserController().handle)
 
 export { router }
